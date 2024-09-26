e54599dcbf0a942ef8a7f25a565d346e6e75a0bd
nikhilreddygv <nguddet1@asu.edu>
2024-09-26 13:37:58 -0500

    first commit

--- untitled/src/com/nikhil/Main.java
+++ untitled/src/com/nikhil/Main.java
@@ -1,4 +1,12 @@
 package com.nikhil;
 
 public class Main {
+
+    public static void main(String[] args) {
+        int [] myIntArray = new int[10];
+        myIntArray[0]= [4,3,2,1];
+
+    }
+
+
 }
