482d0ca7af16fc29a3a74c40e2dcf285b6dd2c7d
nikhilreddygv <nguddet1@asu.edu>
2024-09-26 13:55:38 -0500

    arrays lesson 1 complete

--- ArraysStarting/src/com/nikhil/Main.java
+++ ArraysStarting/src/com/nikhil/Main.java
@@ -3,6 +3,10 @@ package com.nikhil;
 public class Main {
 
     public static void main(String[] args) {
-
+        int[] myIntArray = {6,5,4,3,2,1};
+//        System.out.println(myIntArray[1]);
+        for(int i =0;i<8;i++){
+            System.out.println(myIntArray[i]);
+        }
     }
 }
