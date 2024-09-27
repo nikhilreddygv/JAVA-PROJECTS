1829266c798db899d6d35ba103eddabef327eb63
nikhilreddygv <nguddet1@asu.edu>
2024-09-26 19:06:20 -0500

    Arrays lesson 120 over

--- UsingArrays/src/com/nikhil/Main.java
+++ UsingArrays/src/com/nikhil/Main.java
@@ -1,7 +1,29 @@
 package com.nikhil;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        int[] secondArray = randomArray(10);
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.sort(secondArray);
+        System.out.println(Arrays.toString(secondArray));
+        int[] thirdArray = new int[15];
+        Arrays. fill(thirdArray,69);
+        System.out.println(Arrays.toString(thirdArray));
+        int[] fourthArray =Arrays.copyOf(thirdArray,12);
+        System.out.println(Arrays.toString(fourthArray));
+
 
     }
+     public static int[] randomArray(int length){
+         Random random = new Random();
+         int[] randomArray = new int[length];
+         for(int i = 0; i<length;i++){
+             randomArray[i] = random.nextInt(25,350);
+         }
+         return randomArray;
+     }
 }
