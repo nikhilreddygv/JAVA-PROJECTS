bd0ea6f4aac777307ea299743dca3fc335634e56
nikhilreddygv <nikhilreddyguddeti@gmail.com>
2024-09-27 09:28:52 -0500

    array descending order sort using a while loop

--- ArrayChallenge/src/com/nikhil/Main.java
+++ ArrayChallenge/src/com/nikhil/Main.java
@@ -11,22 +11,42 @@ public class Main {
         for(int i=0; i<lengthofarray;i++){
             randomArray[i]=random.nextInt(11,100);
         }
-        Arrays.sort(randomArray);
+//        Arrays.sort(randomArray);
         return randomArray;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("enter the length of the array you want");
-        int length = scanner.nextInt();
-        int[] reverseArray = generateRandomArray(length);
-        System.out.println("the array that you generated is");
+
+        int[] reverseArray = generateRandomArray(10);
         System.out.println(Arrays.toString(reverseArray));
-        for (int i =0; i<length/2;i++){
-            int temp = reverseArray[i];
-            reverseArray[i]=reverseArray[length-i-1];
-            reverseArray[length-i-1]=temp;
+        boolean flag =true;
+        int temp;
+        while(flag){
+            flag = false;
+            for(int i = 0;i<reverseArray.length-1;i++){
+                if(reverseArray[i]<reverseArray[i+1]){
+                    temp = reverseArray[i];
+                    reverseArray[i]=reverseArray[i+1];
+                    reverseArray[i+1] = temp;
+                    flag =true;
+                }
+            }
         }
-        System.out.println("The above array in descending order is = "+ Arrays.toString(reverseArray));
+        System.out.println(Arrays.toString(reverseArray));
     }
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter the length of the array you want");
+//        int length = scanner.nextInt();
+//        int[] reverseArray = generateRandomArray(length);
+//        System.out.println("the array that you generated is");
+//        System.out.println(Arrays.toString(reverseArray));
+//        for (int i =0; i<length/2;i++){
+//            int temp = reverseArray[i];
+//            reverseArray[i]=reverseArray[length-i-1];
+//            reverseArray[length-i-1]=temp;
+//        }
+//        System.out.println("The above array in descending order is = "+ Arrays.toString(reverseArray));
+//    }
 }
