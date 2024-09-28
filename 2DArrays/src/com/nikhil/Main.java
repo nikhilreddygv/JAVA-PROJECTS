db3e2462cd8afb79c56013086fa5067507f606ac
nikhilreddygv <nikhilreddyguddeti@gmail.com>
2024-09-27 21:27:53 -0500

    started 2d arrays

---
+++ 2DArrays/src/com/nikhil/Main.java
@@ -0,0 +1,17 @@
+package com.nikhil;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int[][] array = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}};
+        System.out.println(Arrays.deepToString(array));
+
+        }
+
+    }
