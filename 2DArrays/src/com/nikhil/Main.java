12d188fe44a6ff99c5c2403d9560109a29012830
nikhilreddygv <nikhilreddyguddeti@gmail.com>
2024-09-27 23:13:16 -0500

    2D arrays finished

--- 2DArrays/src/com/nikhil/Main.java
+++ 2DArrays/src/com/nikhil/Main.java
@@ -10,7 +10,37 @@ public class Main {
                 {1,2,3},
                 {4,5,6},
                 {7,8,9}};
+
+        for (int i = 0;i<array.length;i++){
+            for (int j =0;j<array[i].length;j++){
+                System.out.print(array[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+
+        for(int[] element:array){
+            for(int digit:element){
+                System.out.print(digit);
+
+            }
+            System.out.print("\n");
+        }
+
         System.out.println(Arrays.deepToString(array));
+        for (int i = 0;i<array.length;i++){
+            for (int j =0;j<array[i].length;j++){
+                array[i][j] = (i*10)+(j+1);
+            }
+            System.out.print("\n");
+        }
+        for (int i = 0;i<array.length;i++){
+            for (int j =0;j<array[i].length;j++){
+                System.out.print(array[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
 
         }
 
